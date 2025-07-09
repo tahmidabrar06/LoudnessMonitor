944b01d868b2507cea4831e5f0733b2763736ac0
Tahmid Abrar <tahmidabrar06@gmail.com>
2025-07-09 15:38:25 +1000

    feat: added highpass filter and beep alert

--- loudnessMonitor.py
+++ loudnessMonitor.py
@@ -2,6 +2,9 @@ import math
 import pyaudio
 import audioop
 import sys
+import wave
+from scipy.signal import butter, lfilter
+import numpy
 
 #loudness threshold in dB
 loudnessThreshhold = 60
@@ -25,6 +28,8 @@ x = 10
 chunks_per_second = (RATE // CHUNK) // x
 counter = 0
 
+
+
 #convert rms to decibel
 def rms_to_dB(rms):
     if rms > 0:
@@ -32,18 +37,40 @@ def rms_to_dB(rms):
     else:
         return 1e-10
 
-while True:
-    data = stream.read(CHUNK)
-    stream.write(data)
-    rms = audioop.rms(data, 2)
-    
-    counter += 1
-    if counter >= chunks_per_second:
-        soundLevel = int(rms_to_dB(rms))
-        print(soundLevel)
-        counter = 0
-        if soundLevel > loudnessThreshhold:
-            print("Too loud")
-            sys.exit()
+#alert
+def beep():
+    with wave.open(sys.path[0]+"\\beep.wav", 'rb') as wf:
+        
+        p = pyaudio.PyAudio()
 
+        alertStream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=False) #Output for testing
+
+        while len(data := wf.readframes(CHUNK)):
+            alertStream.write(data)
+        alertStream.close()
+
+#noise filter
+def highpass_filter(audio_bytes, cutoffFreq=200, freq=RATE, order=5):
+    audio_np = numpy.frombuffer(audio_bytes, dtype=numpy.int16)
+    b, a = butter(order, cutoffFreq / (0.5 * freq), btype='high', analog=False)
+    filtered = lfilter(b, a, audio_np)
+    return filtered.astype(numpy.int16).tobytes()
+
+if __name__ == "__main__":
+    while True:
+        data = stream.read(CHUNK)
+        filtered_data = highpass_filter(data)
+        rms = audioop.rms(filtered_data, 2)
+        #stream.write(data) 
+        soundLevel = int(rms_to_dB(rms))
+        counter += 1
+        if counter >= chunks_per_second:
+            print(soundLevel)
+            counter = 0
+            if soundLevel > loudnessThreshhold:
+                print("Too loud")
+                beep()
 
