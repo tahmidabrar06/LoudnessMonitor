11d41b0a61c131f42c299202fc4cb2bb2a47833b
Tahmid Abrar <tahmidabrar06@gmail.com>
2025-07-23 13:27:54 +1000

    change: numpy used for rms calculation instead of audioop (deprecated)

--- loudnessMonitor.py
+++ loudnessMonitor.py
@@ -1,10 +1,10 @@
 import math
 import pyaudio
-import audioop
 import sys
 import wave
 from scipy.signal import butter, lfilter
 import numpy
+import tkinter as tk
 
 class LoudnessMonitor:
     def __init__(self, volume=0.3, loudnessThreshhold=65, rate=44100, chunk=1024, cutoffFreq = 200):
@@ -30,6 +30,10 @@ class LoudnessMonitor:
         self.chunksPerSec = (self.rate // self.chunk) // 10
 
     # Convert RMS to Decibel
+    def calculateRms(self, audio_bytes):
+        samples = numpy.frombuffer(audio_bytes, dtype=numpy.int16).astype(numpy.float32)
+        return numpy.sqrt(numpy.mean(samples ** 2))
+    
     def rms_to_dB(self, rms):
         if rms > 0:
             return 20 * math.log10(rms)
@@ -37,7 +41,7 @@ class LoudnessMonitor:
             return 1e-10
 
     # Set Volume
-    def SetVolume(self, sample):
+    def setVolume(self, sample):
         #Byte to numpy array
         nmArray = numpy.frombuffer(sample, dtype=numpy.int16)
         adjustedAudio = (nmArray * self.volume).astype(numpy.int16)
@@ -53,7 +57,7 @@ class LoudnessMonitor:
                             output=True)
 
             while len(data := wav.readframes(self.chunk)):  
-                alertStream.write(self.SetVolume(data))
+                alertStream.write(self.setVolume(data))
             alertStream.close()
 
     # Noise filter
@@ -63,23 +67,41 @@ class LoudnessMonitor:
         filtered = lfilter(b, a, audio_np)
         return filtered.astype(numpy.int16).tobytes()
 
-    # Start Monitoring Input
+    # Monitor Audio
     def monitor(self):
         counter = 0
-        while True:
+        while self.running == True:
             data = self.stream.read(self.chunk)
             filtered = self.highpass_filter(data)
-            rms = audioop.rms(filtered, 2)
+            rms = self.calculateRms(filtered)
+            self.decibel = int(self.rms_to_dB(rms))
             # Output Audio (For Testing)
             #stream.write(data) 
-            self.decibel = int(self.rms_to_dB(rms))
             counter += 1
             if counter >= self.chunksPerSec:
-                print(self.decibel)
+                if self.decibel >= 0:
+                    print(self.decibel)
+                else:
+                    print("0")
                 counter = 0
                 if self.decibel > self.loudnessThreshhold:
                     print("Too loud")
                     self.beep()
-                    
+    
+    # Start Monitoring
+    def start(self):
+        self.running = True
+        self.monitor()
+
+    # Stop Monitoring
+    def stop(self):
+        self.running = False
+
+class GUI():
+    def __init__(self):
+        self.root = tk.Tk()
+    def StartGUI(self):
+        self.root.mainloop()
 if __name__ == "__main__":
-    LoudnessMonitor().monitor()
+    LoudnessMonitor().start()
+    #GUI().StartGUI()
